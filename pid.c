946822c39241fa574dd0517d0e6c980f9d7ce117
home-coder <one_face@sina.com>
2017-08-06 22:17:44 +0800

    格式化代码

--- pid.c
+++ pid.c
@@ -43,77 +43,77 @@ char *basename(const char *path)
 }
 
 /* find all pid of process by name, only compare base name of pid_name
-  * pid_list: caller malloc pid_t array
-  * list_size: the size of pid_list
-  * RETURN:
-  *        < 0: error number
-  *        >=0: how many pid found, pid_list will store the founded pid
-  */
+ * pid_list: caller malloc pid_t array
+ * list_size: the size of pid_list
+ * RETURN:
+ *        < 0: error number
+ *        >=0: how many pid found, pid_list will store the founded pid
+ */
 int get_pid_by_name(const char* process_name, pid_t pid_list[], int list_size)
 {
 #define  MAX_BUF_SIZE       256
 
-    DIR *dir;
-    struct dirent *next;
-    int count=0;
-    pid_t pid;
-    FILE *fp;
-    char *base_pname = NULL;
-    char *base_fname = NULL;
-    char cmdline[MAX_BUF_SIZE];
-    char path[MAX_BUF_SIZE];
+	DIR *dir;
+	struct dirent *next;
+	int count=0;
+	pid_t pid;
+	FILE *fp;
+	char *base_pname = NULL;
+	char *base_fname = NULL;
+	char cmdline[MAX_BUF_SIZE];
+	char path[MAX_BUF_SIZE];
 
-    if(process_name == NULL || pid_list == NULL)
-        return -EINVAL;
+	if(process_name == NULL || pid_list == NULL)
+		return -EINVAL;
 
-    base_pname = basename(process_name);
-    if(strlen(base_pname) <= 0)
-        return -EINVAL;
+	base_pname = basename(process_name);
+	if(strlen(base_pname) <= 0)
+		return -EINVAL;
 
-    dir = opendir("/proc");
-    if (!dir)
-    {
-        return -EIO;
-    }
-    while ((next = readdir(dir)) != NULL) {
-        /* skip non-number */
-        if (!isdigit(*next->d_name))
-            continue;
+	dir = opendir("/proc");
+	if (!dir)
+	{
+		return -EIO;
+	}
+	while ((next = readdir(dir)) != NULL) {
+		/* skip non-number */
+		if (!isdigit(*next->d_name))
+			continue;
 
-        pid = strtol(next->d_name, NULL, 0);
-        sprintf(path, "/proc/%u/cmdline", pid);
-        fp = fopen(path, "r");
-	if(fp == NULL)
-		continue;
+		pid = strtol(next->d_name, NULL, 0);
+		sprintf(path, "/proc/%u/cmdline", pid);
+		fp = fopen(path, "r");
+		if(fp == NULL)
+			continue;
 
-        memset(cmdline, 0, sizeof(cmdline));
-        if(fread(cmdline, MAX_BUF_SIZE - 1, 1, fp) < 0){ 
-            fclose(fp);
-            continue;
-        }
-        fclose(fp);
-        base_fname = basename(cmdline);
+		memset(cmdline, 0, sizeof(cmdline));
+		if(fread(cmdline, MAX_BUF_SIZE - 1, 1, fp) < 0){ 
+			fclose(fp);
+			continue;
+		}
+		fclose(fp);
+		base_fname = basename(cmdline);
 
-        if (strcmp(base_fname, base_pname) == 0 )
-        {
-            if(count >= list_size){
-                break;
-            }else{
-                pid_list[count] = pid;
-                count++;
-            }
-        }
-    }
-    closedir(dir) ;
-    return count;
+		if (strcmp(base_fname, base_pname) == 0 )
+		{
+			if(count >= list_size){
+				break;
+			}else{
+				pid_list[count] = pid;
+				count++;
+			}
+		}
+	}
+	closedir(dir) ;
+	return count;
 }
 
 /* If process is existed, return true */
 int is_process_exist(const char* process_name)
 {
-    pid_t pid;
+	pid_t pid;
 
-    return (get_pid_by_name(process_name, &pid, 1) > 0);
+	return (get_pid_by_name(process_name, &pid, 1) > 0);
 }
 
 #define MAX_PID_NUM 64
